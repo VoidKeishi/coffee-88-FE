6cabfdd3eba902a53b9fb47623b8da7e0e74d424
kientran1507 <trntrgkn@gmail.com>
2024-11-28 02:37:43 +0700

    Fetch product data in cafes.json and pass it to child components (Header, Slider)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import Header from './components/header/Header';
 import Slider from './components/Slider/Slider';
 import './App.css'
@@ -5,17 +6,31 @@ import Filter from './components/filter/Filter';
 import ShopInfoCard from './components/shop-info-card/ShopInfoCard';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch the data
+    fetch('/cafes.json')
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((error) => {
+        console.error('Error loading JSON data:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <Header />
+      <Header products={products}/>
       <Filter />
       <div className='slider-container'>
         <h3>Đang hot</h3>
-        <Slider />
+        <Slider products={products}/>
       </div>
       <div className='slider-container'>
         <h3>Gần đây</h3>
-        <Slider />
+        <Slider products={products}/>
       </div>
     </div>
   );
