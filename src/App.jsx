74ad6986472e30043e1b1cc38e6173f51fa6393f
Ezzio103 <tuancho1003hn@gmail.com>
2024-12-09 16:42:04 +0700

    detail, search - 1209 - DucNT

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { Register } from './pages/register';
 import Display from './pages/display';
 import ForgotPassword from './pages/forgot-password';
 import Personal from './pages/personal';
+import Detail from './pages/detail';
+import Search from './pages/search';
 import './App.css';
 
 function App() {
@@ -15,16 +17,9 @@ function App() {
             <Route path="/display" element={<Display />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/personal" element={<Personal />} />
-            <Route
-                path="/"
-                element={
-                    <Navigate
-                        to="/display
-            "
-                        replace
-                    />
-                }
-            />
+            <Route path="/detail" element={<Detail />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/" element={<Navigate to="/display" replace />} />
         </Routes>
     );
 }
